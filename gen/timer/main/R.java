7bfb8c368a6aa6873810f646e2edc5dbdda425b9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-06-27 15:37:52 +0900

    B=B1
    v=1
    d=1
    
    [d]-----------------------------------
    1. android:background="@drawable/button_forward"
    	==> Layout irregular(see => "C:\WORKS\WORKSPACES_ANDROID\S_01_Timer\res\drawable")
    
    [n]-----------------------------------
    1. Buttons => Let's use texts

--- gen/timer/main/R.java
+++ gen/timer/main/R.java
@@ -11,18 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int draw_02_opaque_button_2=0x7f020000;
-        public static final int draw_02_opaque_button_3=0x7f020001;
-        public static final int draw_02_opaque_button_4=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int set_stroke_width_3=0x7f020004;
+        public static final int button_background_drawable=0x7f020000;
+        public static final int button_backward=0x7f020001;
+        public static final int button_forward=0x7f020002;
+        public static final int draw_02_opaque_button_2=0x7f020003;
+        public static final int draw_02_opaque_button_3=0x7f020004;
+        public static final int draw_02_opaque_button_4=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int set_stroke_width_3=0x7f020007;
     }
     public static final class id {
         public static final int buttonSettings=0x7f050003;
-        public static final int buttonStart=0x7f050005;
-        public static final int buttonStop=0x7f050006;
+        public static final int buttonStart=0x7f050007;
+        public static final int buttonStop=0x7f050008;
+        public static final int button_progress_backward=0x7f050005;
+        public static final int button_progress_forward=0x7f050004;
         public static final int linearLayout1=0x7f050000;
-        public static final int linearLayout2=0x7f050004;
+        public static final int linearLayout2=0x7f050006;
         public static final int seekBar1=0x7f050002;
         public static final int textView1=0x7f050001;
     }
