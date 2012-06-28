fa57b67209dd0f0531bd1579370551b5c591d022
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-06-28 12:32:53 +0900

    B=B3
    v=1
    d=2
    
    [d]-----------------------------------
    1. NOP
    
    [n]-----------------------------------
    1. Halt this branch. Let's move on building a timer app

---
+++ src/helper/main/FlingView.java
@@ -0,0 +1,13 @@
+package helper.main;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class FlingView extends LinearLayout {
+
+	public FlingView(Context context) {
+		super(context);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}//public class FlingView extends TextView
